ae8b0e5dc96df1050e2ebccebf00630427dd7b7f
rygrit <ry_grit@sina.com>
2026-03-11 22:15:08 +0800

    feat: support catalog completion

---
+++ src/providers/completion-item/catalog.ts
@@ -0,0 +1,37 @@
+import type { CompletionItemProvider, Position, TextDocument } from 'vscode'
+import { getResolvedDependencyByOffset, getWorkspaceContext } from '#core/workspace'
+import { offsetRangeToRange } from '#utils/ast'
+import { CompletionItem, CompletionItemKind } from 'vscode'
+
+export class CatalogCompletionItemProvider implements CompletionItemProvider {
+  static triggers = [':']
+
+  async provideCompletionItems(document: TextDocument, position: Position) {
+    const offset = document.offsetAt(position)
+    const info = await getResolvedDependencyByOffset(document.uri, offset)
+    if (!info?.rawSpec.startsWith('catalog:'))
+      return
+
+    const ctx = await getWorkspaceContext(document.uri)
+    if (!ctx)
+      return
+
+    const catalogs = await ctx.getCatalogs()
+
+    if (!catalogs)
+      return
+
+    const items: CompletionItem[] = []
+
+    for (const name in catalogs) {
+      const item = new CompletionItem(name, CompletionItemKind.EnumMember)
+
+      item.range = offsetRangeToRange(document, info.specRange)
+      item.insertText = name
+
+      items.push(item)
+    }
+
+    return items
+  }
+}
